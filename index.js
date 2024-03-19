571d88ce62b31c3929870d2f8cf785bbf2fd104d
AntonIvan0v <anton.ivanov.se@gmail.com>
2024-03-19 13:40:12 +0200

    додає кнопку скрола та favicon

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.querySelector(".button-scroll-up").style.display = "block";
+    } else {
+        document.querySelector(".button-scroll-up").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+};
+
+const buttonScrollUp = document.querySelector(".button-scroll-up");
+buttonScrollUp.addEventListener('click', topFunction);
+
+
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
@@ -15,3 +35,4 @@
     refs.menu.classList.toggle('is-open');
   }
 })();
+
